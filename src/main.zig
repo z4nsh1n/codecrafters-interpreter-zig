ef84b9ab3302e82b4fa3efd283a59f19907bc20b
z4nshin <z4nshin@gmail.com>
2025-04-09 23:17:00 +0200

    Assignment & equality 3

--- src/main.zig
+++ src/main.zig
@@ -94,9 +94,8 @@ pub fn main() !void {
                 try t.to_string();
             } else if (std.mem.eql(u8, token, "=")) {
                 // peek next
-                const eq = file_contents[idx + 1 .. idx + 2];
                 var t: Token = undefined;
-                if (idx + 2 < file_contents.len and std.mem.eql(u8, eq, "=")) {
+                if (idx + 2 < file_contents.len and std.mem.eql(u8, file_contents[idx + 1 .. idx + 2], "=")) {
                     t = Token{ .ttype = .EQUAL_EQUAL, .lexeme = token, .line = line, .object = null };
                     idx += 1;
                 } else {
